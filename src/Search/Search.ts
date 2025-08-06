46c14784d1bc25130e25e1acfd75ab67f3436825
Luis <luisbarbosa.santos@outlook.com>
2025-08-06 06:31:30 -0300

    add aux llm to filter results

--- src/Search/Search.ts
+++ src/Search/Search.ts
@@ -1,14 +1,44 @@
 import 'dotenv/config';
 import { loadDocuments} from '../loader/loader'
+import { ChatOllama } from "@langchain/ollama";
+import { PromptTemplate } from "@langchain/core/prompts";
 
-export async function searchChunks(query : string) {
+export const minillmModel = new ChatOllama({
+    model: "qwen2:1.5b",
+    temperature: 0.2,
 
-    
-    console.log("Modelo de re-ranking carregado")
+});
+
+export async function searchChunks(query : string): Promise<any> {
+    const templatePrepareDocument = `Você é um especialista em processamento e filtragem de informações. Sua tarefa é extrair e reter APENAS os trechos de um CONTEXTO fornecido que são diretamente relevantes para responder à PERGUNTA do usuário.
+    Siga estas regras rigorosamente:
+    1.Leia a question para entender qual informação específica é necessária.
+    2.Leia o contexto e identifique todas as frases ou parágrafos que contêm a informação necessária para responder à question.
+    3.Filtre e descarte qualquer informação, frase ou parágrafo que seja ruído ou que não ajude a responder diretamente à pergunta.
+    4.Retorne APENAS os trechos relevantes que você extraiu.
+    5.Mantenha a linguagem original dos trechos extraídos, sem reescrevê-los.
+    6.Se nenhum trecho do contexto for relevante para a pergunta, retorne o contexto original"
+
+    contexto:
+    {context}
+    question:
+    {question}`;
+
+    const promptPrepare = new PromptTemplate({
+        template: templatePrepareDocument,
+        inputVariables: ["context", "question"]
+    });
+    const chain = promptPrepare.pipe(minillmModel);
 
     const vectorStore = await loadDocuments();
     //Find the first 20 similar chunks
-    const resultsnoFiltered =  await vectorStore.similaritySearchWithScore(query, 20);
-    return resultsnoFiltered;
+    const resultsnoFiltered =  await vectorStore.similaritySearchWithScore(query, 15);
+
+    const response = await chain.invoke({
+        context: resultsnoFiltered.map(doc =>  doc[0].pageContent).join("/n"),
+        question: query,
+    });
+
+    return response.content;
     
 }
--- src/services/answer.services.ts
+++ src/services/answer.services.ts
@@ -1,7 +1,6 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { searchChunks } from "../Search/Search";
-// import { PromptTemplate } from "@langchain/core/prompts";
-import { pipeline } from "@xenova/transformers";
+import { PromptTemplate } from "@langchain/core/prompts";
 
 export const llmModel = new ChatGoogleGenerativeAI({
     model: "gemini-1.5-flash",
@@ -12,37 +11,33 @@ export const llmModel = new ChatGoogleGenerativeAI({
 
 export class mainServices {
     async manageSearch(answer: string): Promise<any> {
-        const crossEncoder = await pipeline('text-classification', 'Xenova/cross-encoder-ms-marco-MiniLM-L-6-v2');
-        const contextChunks = await searchChunks(answer);
-        // Get the document content of metadata chunks
-        const documentsContent = contextChunks.map(doc => doc[0].pageContent);
-        const questions = contextChunks.map(() =>  answer)
-        console.log("Questions:", questions);
-        console.log("Documents content;", documentsContent);
-        // Classify the document chunks
-        const contextResultsAfterScore = await crossEncoder(questions, { text_pair : documentsContent} as any);
 
-        console.log(contextResultsAfterScore);
-        
+        // const contextChunks = await searchChunks(answer);
+        // // Get the document content of metadata chunks
+        // const documentsContent = contextChunks.map(doc => doc[0].pageContent);
+
+        const context = await searchChunks(answer);
+        console.log(context)
         //Prompt
-        // const templatePrompt = `Você é um assistente especialista que responde a perguntas com base em um contexto fornecido, utilize o contexto fornecido para responder a pergunta da melhor forma possivel, caso a pergunta não esteja inclusa no contexto, tente responder com base em algum padrão ou exemplo fornecido pelo contexto, caso ainda não exista responda que não sabe.
-        // Contexto:
+        const templatePrompt = `Você é um assistente especialista que responde a perguntas com base em um contexto fornecido, utilize o contexto fornecido para responder a pergunta da melhor forma possivel, caso a pergunta não esteja inclusa no contexto, tente responder com base em algum padrão ou exemplo fornecido pelo contexto, caso ainda não exista responda que não sabe.
+        Contexto:
 
-        // {context}
+        {context}
 
-        // Pergunta:
-        // {question}`;
+        Pergunta:
+        {question}`;
 
-        // const prompt = new PromptTemplate({
-        //     template: templatePrompt,
-        //     inputVariables: ["context", "question"]
-        // });
+        const prompt = new PromptTemplate({
+            template: templatePrompt,
+            inputVariables: ["context", "question"]
+        });
+        
+        const chain = prompt.pipe(llmModel);
 
-        // const chain = prompt.pipe(llmModel);
-        // const response = await chain.invoke({
-        //     context: context,
-        //     question: answer,
-        // })
-        // return response.content
+        const response = await chain.invoke({
+            context: context,
+            question: answer,
+        })
+        return response.content;
     }
 }
